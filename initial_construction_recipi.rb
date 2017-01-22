0b6a2767d74d3e642d13fd991d5d376ead4996b5
HirofumiYamamoto <hirofumiymmto@gmail.com>
2017-01-22 18:57:18 +0900

    use package

--- initial_construction_recipi.rb
+++ initial_construction_recipi.rb
@@ -1,45 +1,13 @@
 REROAD_PROFILE = ". ~/.profile"
 execute "ubuntu update" do
-  command "sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade"
+  command "sudo apt-get -y update"
 end
-execute "install packages required for rbenv" do
-  command "sudo apt-get -y install git build-essential libssl-dev"
-end
-execute "rbenv install" do
-  not_if "ls ~/.rbenv"
-  command "git clone https://github.com/sstephenson/rbenv.git ~/.rbenv && git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
-end
-execute "chown rbenv" do
-  command "sudo chown -R ubuntu:ubuntu ~/.rbenv"
-end
-execute "update .profile for rbenv" do
-  not_if %(#{REROAD_PROFILE} && rbenv --help)
-  command %(sudo echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile && sudo echo 'eval "$(rbenv init -)"' >> ~/.profile)
-end
-execute "reload .profile for rbenv" do
-  command ". ~/.profile"
-end
-execute "install packages required for ruby" do
-  command "sudo apt-get install -y libreadline-dev"
-end
-execute "install ruby" do
-  command "#{REROAD_PROFILE} && rbenv install -s 2.3.3"
-end
-execute "apply rbenv specific version" do
-  command "#{REROAD_PROFILE} && rbenv global 2.3.3"
-end
-execute "install bundler" do
-  command "#{REROAD_PROFILE} && rbenv exec gem install bundler"
-end
-execute "install libsqlite3 for sqlite3" do
-  command "sudo apt-get -y install libsqlite3-dev"
-end
-execute "install sqlite3 for rails" do
-  command %(#{REROAD_PROFILE} && rbenv exec gem install sqlite3)
-end
-execute "install nginx" do
-  command "sudo apt-get -y install nginx"
-end
-execute "install rails" do
-  command %(#{REROAD_PROFILE} && rbenv exec gem install rails)
-end
+package 'build-essential'
+package 'libssl-dev'
+package 'libreadline-dev'
+package 'git'
+package 'nginx'
+package 'ruby-dev'
+package 'libsqlite3-dev'
+gem_package 'sqlite3'
+gem_package 'rails'
