c037ea27d2612eed6f1ded0e206ea3dcb129305e
HirofumiYamamoto <hirofumiymmto@gmail.com>
2017-01-22 15:26:31 +0900

    install rbenv and ruby

--- initial_construction_recipi.rb
+++ initial_construction_recipi.rb
@@ -1,11 +1,32 @@
+RBENV_PATH = %(export PATH="$HOME/.rbenv/bin:$PATH")
 execute "ubuntu update" do
   command "sudo apt-get -y update"
   command "sudo apt-get -y upgrade"
+  command "sudo apt-get -y dist-upgrade"
 end
-execute "install nginx" do
-  command "sudo apt-get -y install nginx"
+execute "install packages required for rbenv" do
+  command "sudo apt-get -y install git build-essential libssl-dev"
 end
 execute "rbenv install" do
-  command "sudo apt-get -y install git build-essential libssl-dev"
-  command "sudo apt-get -y install rbenv"
+  not_if "ls ~/.rbenv"
+  command "git clone https://github.com/sstephenson/rbenv.git ~/.rbenv && git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
+end
+execute "chown rbenv" do
+  command "chown -R ubuntu:ubuntu ~/.rbenv"
+end
+execute "update .profile for rbenv" do
+  not_if %(#{RBENV_PATH} && rbenv --help)
+  command %(sudo echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile && sudo echo 'eval "$(rbenv init -)"' >> ~/.profile)
+end
+execute "reload .profile for rbenv" do
+  command ". ~/.profile"
+end
+execute "install packages required for ruby" do
+  command "sudo apt-get install -y libreadline-dev"
+end
+execute "install ruby" do
+  command "#{RBENV_PATH} && rbenv install -s 2.3.3"
+end
+execute "apply rbenv specific version" do
+  command "#{RBENV_PATH} && rbenv global 2.3.3"
 end
